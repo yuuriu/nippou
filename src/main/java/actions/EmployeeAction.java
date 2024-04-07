00922af19037184325fcaf12848b0efdcc6ddbcc
yuuto.ueda <kakegawayuuri@gmail.com>
2024-04-07 16:50:08 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -64,4 +64,13 @@ public class EmployeeAction extends ActionBase {
 
     }
 
+    public void entryNew() throws ServletException, IOException {
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
+    }
+
 }

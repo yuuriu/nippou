6fd1f57aaf1ceab8a4ecf9b98f381da1328d658f
yuuto.ueda <kakegawayuuri@gmail.com>
2024-04-07 16:34:49 +0900

    EmployeeValidator、EmployeeService作成

---
+++ src/main/java/services/ServiceBase.java
@@ -0,0 +1,15 @@
+package services;
+
+import javax.persistence.EntityManager;
+
+import utils.DBUtil;
+
+public class ServiceBase {
+    protected EntityManager em = DBUtil.createEntityManager();
+
+    public void close() {
+        if (em.isOpen()) {
+            em.close();
+        }
+    }
+}

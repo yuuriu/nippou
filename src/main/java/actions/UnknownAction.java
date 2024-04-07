6569c4ab10c5659161673dc3997741b71e09a9f6
yuuto.ueda <kakegawayuuri@gmail.com>
2024-04-07 15:44:52 +0900

    FrontController作成

---
+++ src/main/java/actions/UnknownAction.java
@@ -0,0 +1,16 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import constants.ForwardConst;
+
+public class UnknownAction extends ActionBase {
+
+    @Override
+    public void process() throws ServletException, IOException {
+        forward(ForwardConst.FW_ERR_UNKNOWN);
+    }
+}
+
